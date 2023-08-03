ac24c478048a57322796848036a1e1112c45018e
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-08-03 13:17:40 +0100

    Create react app to simulate AccoAI env

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, World!</h1>
+        <p>I am <strong>Acco AI Mail Reader</strong></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
